64316fc6d305c8dbdf92a5609575ae2d326b999f
Joseph Suvak <robert.suvak@hcahealthcare.com>
2021-11-06 17:59:55 -0500

    Initial commit

---
+++ farcry-java-api/src/main/java/com/farcryJava/FarcryJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.farcryJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarcryJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarcryJavaApiApplication.class, args);
+	}
+
+}
---
+++ farcry-java-api/src/test/java/com/farcryJava/FarcryJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farcryJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarcryJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
